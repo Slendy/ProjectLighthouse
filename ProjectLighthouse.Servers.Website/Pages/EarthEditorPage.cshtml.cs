924fd74ad13795c01259f0582d09c5fb4df6ffdf
Slendy <josh@slendy.pw>
2023-03-18 20:36:32 -0500

    Add proof of concept Didn't turn out as well as I'd hoped

--- ProjectLighthouse.Servers.Website/Pages/EarthEditorPage.cshtml.cs
+++ ProjectLighthouse.Servers.Website/Pages/EarthEditorPage.cshtml.cs
@@ -1,7 +1,11 @@
 ﻿using LBPUnion.ProjectLighthouse.Database;
 using LBPUnion.ProjectLighthouse.Servers.Website.Pages.Layouts;
+using LBPUnion.ProjectLighthouse.Servers.Website.Types;
+using LBPUnion.ProjectLighthouse.Types.Entities.Level;
 using LBPUnion.ProjectLighthouse.Types.Entities.Profile;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace LBPUnion.ProjectLighthouse.Servers.Website.Pages;
 
@@ -9,7 +13,9 @@ public class EarthEditorPage : BaseLayout
 {
     private readonly DatabaseContext database;
 
-    public User ProfileUser { get; set; } = null!;
+    public User? ProfileUser { get; set; }
+
+    public UserEarth UserEarth { get; set; } = null!;
 
     public EarthEditorPage(DatabaseContext database) : base(database)
     {
@@ -24,6 +30,20 @@ public class EarthEditorPage : BaseLayout
         if (this.User == null) return this.Redirect("~/");
 
         if (this.User != this.ProfileUser && !this.User.IsModerator) return this.Redirect("~/");
+
+        this.ProfileUser = await this.database.Users.FindAsync(userId);
+
+        if (this.ProfileUser == null) return this.Redirect("~/");
+
+        this.UserEarth = new UserEarth();
+
+        this.UserEarth.AddUserLocation(this.ProfileUser.Location);
+
+        foreach(Slot s in await this.database.Slots.Where(s => s.CreatorId == this.ProfileUser.UserId).ToListAsync())
+        {
+            Console.WriteLine($"{s.Name} - {s.Location.X}, {s.Location.Y} - {s.GameVersion} - {s.CreatorId}");
+            this.UserEarth.AddLevelLocation(s.Location);
+        }
 
         //TODO fetch all user slot locations and profile card location
         return this.Page();
---
+++ ProjectLighthouse.Servers.Website/Types/UserEarth.cs
@@ -0,0 +1,22 @@
+﻿using LBPUnion.ProjectLighthouse.Types.Misc;
+
+namespace LBPUnion.ProjectLighthouse.Servers.Website.Types;
+
+public enum LocationType
+{
+    Profile,
+    Level,
+}
+
+public class UserEarth
+{
+
+    public Dictionary<Location, LocationType> Locations = new();
+
+    public Location GetProfileLocation() => this.Locations.First(l => l.Value == LocationType.Profile).Key;
+
+    public void AddUserLocation(Location l) => this.Locations.Add(l, LocationType.Profile);
+
+    public void AddLevelLocation(Location l) => this.Locations.Add(l, LocationType.Level);
+
+}
